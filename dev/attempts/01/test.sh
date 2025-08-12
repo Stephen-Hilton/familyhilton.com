cbb387246473a1e308b8100bde768280e0e8361f
Stephen-Hilton <stephen@spaceandtime.io>
2025-08-11 18:44:28 -0700

    fixed the lower() issue with author link in blog posts

--- dev/attempts/01/test.sh
+++ dev/attempts/01/test.sh
@@ -1,15 +1,5 @@
-python3 -m venv sitegen/venv_01
-source sitegen/venv_01/bin/activate
-pip install pyyaml jinja2 markdown
-pip install --upgrade pip
-
-python3 -m sitegen build --root ./root  # writes /root/index.html and /root/pages/**
-
-export PUBPATH="../../../docs"
-mkdir -p ${PUBPATH}        # create parent directory if it doesn't exist
-setopt rm_star_silent
-rm -rf ${PUBPATH}/*     # clear parent directory (to trash)
-cp -R root/* ${PUBPATH} # copy to parent directory
+#!/bin/bash
+. ./build.sh
 
 # Serve the content using Python's built-in HTTP server, for local testing
 python3 -m http.server 8000 --directory ${PUBPATH}
